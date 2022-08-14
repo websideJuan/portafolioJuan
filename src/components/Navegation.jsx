ae1b13ab6207f8bb8c23fe5f0d3f4c92f8d24d6e
juanHerrera <portador1051@gmail.com>
2022-08-14 10:11:13 -0400

    correccion menu

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,28 +1,15 @@
-import { useState } from "react"
 import NavegationUl from "./NavegationUl.jsx"
 
 
 const Navegation = () => {
-  const [contador, setContador] = useState(false)
-  const menuShow = () => {setContador(!contador)}
-  console.log(contador)
+
+  
   return (
     <nav className="navegacion">
       <div className="nav__logo">
         <p className="nav__logo-para">Portafolio</p>
       </div>
-      <NavegationUl props={contador}/> 
-      <div className="icons__menu">
-        <button className="cta cta-search">
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
-        <button
-          onClick={() => menuShow()} 
-          className="cta cta-menu"
-        >
-          <i className="fa-solid fa-bars"></i>
-        </button>
-      </div>
+      <NavegationUl/> 
     </nav>
   )
 }
--- src/components/NavegationUl.jsx
+++ src/components/NavegationUl.jsx
@@ -1,30 +1,47 @@
+import { useState } from "react"
 
-const NavegationUl = ({contador}) => {
-    
-    console.log(contador)
+const NavegationUl = () => {
+
+
+  const [contador, setContador] = useState(false)
+
+
     return (
-    <ul className={`nav__center ${contador ? null:"showMenu"}`}>
-        <li className="nav-item">
-            <i className="fa-solid fa-house"></i>
-            <a href="#/" className="nav-link">Inicio</a>
-        </li>
-        <li className="nav-item">
-            <i className="fa-solid fa-circle-info"></i>
-            <a href="#/" className="nav-link">Aserca de mi</a>
-        </li>
-        <li className="nav-item">
-            <i className="fa-solid fa-toolbox"></i>
-            <a href="#/" className="nav-link">habilidades</a>
-        </li>
-        <li className="nav-item">
-            <i className="fa-solid fa-diagram-project"></i>
-            <a href="#/" className="nav-link">trabajos</a>
-        </li>
-        <li className="nav-item">
-            <i className="fa-solid fa-address-card"></i>
-            <a href="#/" className="nav-link">contactame!</a>
-        </li>
-    </ul>
+        <>
+            <ul className={`nav__center ${contador ? 'd-none':''}`}>
+                <li className="nav-item">
+                    <i className="fa-solid fa-house"></i>
+                    <a href="#/" className="nav-link">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <i className="fa-solid fa-circle-info"></i>
+                    <a href="#/" className="nav-link">Aserca de mi</a>
+                </li>
+                <li className="nav-item">
+                    <i className="fa-solid fa-toolbox"></i>
+                    <a href="#/" className="nav-link">habilidades</a>
+                </li>
+                <li className="nav-item">
+                    <i className="fa-solid fa-diagram-project"></i>
+                    <a href="#/" className="nav-link">trabajos</a>
+                </li>
+                <li className="nav-item">
+                    <i className="fa-solid fa-address-card"></i>
+                    <a href="#/" className="nav-link">contactame!</a>
+                </li>
+            </ul>
+            <div className="icons__menu">
+                <button className="cta cta-search">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <button
+                    onClick={() => setContador(!contador)} 
+                    className="cta cta-menu"
+                >
+                        <i className="fa-solid fa-bars"></i>
+                </button>
+            </div>
+        </>
   )
 }
 
